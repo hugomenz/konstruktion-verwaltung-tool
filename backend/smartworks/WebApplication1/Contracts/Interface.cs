700d207bcd345a5bbe3c3e6acf9bf4fd5ba14f9c
Hugo Martin <hugomartin.menz@gmail.com>
2023-01-21 19:48:46 +0100

    Added .NET API

---
+++ backend/smartworks/WebApplication1/Contracts/Interface.cs
@@ -0,0 +1,9 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Contracts
+{
+    public interface IProjectRepository
+    {
+        List<Project> GetProjectList();
+    }
+}
---
+++ backend/smartworks/WebApplication1/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class Task
+    {
+        public string id { get; set; }
+        public string description { get; set; }
+        public string date { get; set; }
+        public string type { get; set; }
+        public int priority { get; set; }
+        public string user { get; set; }
+        public int projectNumber { get; set; }
+        public bool isCompleted { get; set; }
+    }
+}
